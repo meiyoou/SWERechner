928ccfb0a15eeb2a95c9808e0463fb72420279ef
Katerina Papadopoulou <106772055+Kateeriina@users.noreply.github.com>
2024-04-17 00:38:13 +0200

    Delete src directory

--- src/operation/AdditionOperation.java
+++
@@ -1,9 +0,0 @@
-package operation;
-
-public class AdditionOperation{
-    public AdditionOperation(){};
-
-    public static double addNumbers(double numberOne, double numberTwo){
-        return numberOne + numberTwo;
-    }
-}
--- src/operation/SubtractionOperation.java
+++
@@ -1,9 +0,0 @@
-package operation;
-
-public class SubtractionOperation {
-    public SubtractionOperation() {}
-
-    public static double subtractNumbers(double numberOne, double numberTwo) {
-        return numberOne - numberTwo;
-    }
-}
