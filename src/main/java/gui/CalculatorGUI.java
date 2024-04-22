690b5d61bad0087ea728778917b052b2488edd85
ikram <ikram.idderhem@gmail.com>
2024-04-23 00:02:22 +0200

    -

--- src/main/java/gui/CalculatorGUI.java
+++ src/main/java/gui/CalculatorGUI.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.function.DoubleUnaryOperator;
+
 import java.util.List;
 import java.util.ArrayList;
 
